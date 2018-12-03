7191aedb6b0aac1b012a91309d7cd7b14fe00511
Ivan <ivankoy@gmail.com>
2018-12-03 07:14:38 +0100

    Devember Day 2

--- win32_devember.cpp
+++ win32_devember.cpp
@@ -9,6 +9,21 @@
 #include <windows.h>
 #include "devember_platform.cpp"
 
+LRESULT CALLBACK
+MainWindowProcedure(HWND Window,
+                    UINT Message,
+                    WPARAM wParam,
+                    LPARAM lParam)
+{
+    switch (Message)
+    {
+    case WM_PAINT:
+    {
+    }
+    break;
+    }
+}
+
 int CALLBACK
 WinMain(HINSTANCE Instance,
         HINSTANCE PrevInstance,
@@ -16,10 +31,15 @@ WinMain(HINSTANCE Instance,
         int CmdShow)
 {
     WNDCLASS WindowClass = {};
+    WindowClass.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW;
+    WindowClass.hInstance = Instance;
+    WindowClass.lpfnWndProc = MainWindowProcedure;
 
+#if 0
     MessageBoxA(0, "Hello world!", "Hello", MB_OK|MB_ICONINFORMATION);
     MessageBoxA(0, "This is another message box!", "Hello", MB_OK|MB_ICONINFORMATION);
     MessageBoxA(0, "This is not an error.", "Error", MB_OK|MB_ICONERROR);
+#endif
 
     return (0);
 }
